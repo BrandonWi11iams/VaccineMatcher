f6b0551319d62fcd221b9ba473e13d85e8f9ee9a
Brandon Williams <bw73069@srcs.org>
2020-12-16 10:21:59 -0800

    cells

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -1,10 +1,12 @@
 vacMatch = {
+  squares: null,
+
 
   init: function() {
     for (let i = 0; i < 16; i++) {
-      let playGrid = document.createElement("div");
-      playGrid.className = "score-container";
-      playGrid_container.appendChild(playGrid);
+      let cells = document.createElement("div");
+      cells.className = "score-container";
+      cells_container.appendChild(cells);
     }
   }
 
