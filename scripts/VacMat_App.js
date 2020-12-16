a066cfb066a0b7fca68b2d2593aff730536edc8a
miguelangeltanem <mt73142@srcs.org>
2020-12-16 08:54:48 -0800

    testing background

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -1,15 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
   const gridDisplay = document.querySelector('.grid')
   const scoreDisplay = document.querySelector('score')
-},
-
-  VacMat = {
-
-    init: function () {
-      let squares_container = document.getElementById("grid");
-    }
-
-  },
-
-
-  VacMat.init();
+})
