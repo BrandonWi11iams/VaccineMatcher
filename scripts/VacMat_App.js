8f0de3dc44c9693babc8d651544797e8ace7a39b
Brandon Williams <bw73069@srcs.org>
2020-12-16 11:30:43 -0800

    Cell container

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -3,6 +3,7 @@ vacMatch = {
 
 
   init: function() {
+    let cells_container = document.getElementById("grid");
     for (let i = 0; i < 16; i++) {
       let cells = document.createElement("div");
       cells.className = "score-container";
