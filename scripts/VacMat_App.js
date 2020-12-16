e988b41edae666e7b9cb5f72554f9ed88c04341e
Brandon Williams <bw73069@srcs.org>
2020-12-16 11:37:05 -0800

    Pls work

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -6,8 +6,7 @@ vacMatch = {
     let cells_container = document.getElementById("grid");
     for (let i = 0; i < 16; i++) {
       let cells = document.createElement("div");
-      cells.className = "score-container";
-      cells_container.appendChild(cells);
+      
     }
   }
 
