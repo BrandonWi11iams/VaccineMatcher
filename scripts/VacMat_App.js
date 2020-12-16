4fe6e76da8738aed681a74d2f7af3f8e5e56e32d
Brandon Williams <bw73069@srcs.org>
2020-12-16 11:48:23 -0800

    Grid attempt

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -6,10 +6,21 @@ vacMatch = {
     let cells_container = document.getElementById("grid");
     for (let i = 0; i < 16; i++) {
       let cells = document.createElement("div");
+      cells.className = "cell";
+      cells_container.appendChild(square);
+    }
+    this.squares = squares_container.children;
+
       
     }
+
+    
   }
 
-}
+
 
 vacMatch.init();
+
+
+
+
