602d772e41e429a3ff991299d7113b2fc792f0bd
Brandon Williams <bw73069@srcs.org>
2020-12-16 10:16:23 -0800

    Grid try

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -1,10 +1,10 @@
 vacMatch = {
 
   init: function() {
-    for (let i = 0; i < 9000; i++) {
-      let square = document.createElement("div");
-      square.className = "square";
-      squares_container.appendChild(square);
+    for (let i = 0; i < 16; i++) {
+      let playGrid = document.createElement("div");
+      playGrid.className = "score-container";
+      playGrid_container.appendChild(playGrid);
     }
   }
 
