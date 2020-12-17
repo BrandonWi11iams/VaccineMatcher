3bb073212efc138011a93d66c691bbf353e08571
Brandon Williams <bw73069@srcs.org>
2020-12-16 16:25:11 -0800

    work pleasse

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -3,7 +3,9 @@ vacMatch = {
 
 
   init: function() {
-    let cells_container = document.getElementById("grid");
+    let cells_container = document.getElementById("grid");{
+
+    }
     for (let i = 0; i < 16; i++) {
       let cells = document.createElement("div");
       cells.className = "cells";
