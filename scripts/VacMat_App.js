12e0d91b24ffc3e005774e2be82488ee59c89a91
miguelangeltanem <mt73142@srcs.org>
2020-12-17 10:18:59 -0800

    block

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -16,7 +16,9 @@ let VacMatApp = {
     this.cells = cells_container.children;
 
     for (let i = 0; i < 2; i++) {
+      this.chemical.push(this.createchem())
     }
+    this.renderchemical();
 
     
   },
@@ -32,6 +34,20 @@ let VacMatApp = {
     }
     return chemical;
   },
+  renderchemical: function () {
+    for (let i = 0; i < this.chemical.length; i++) {
+      this.chemical[i].element.style.top = this.chemical[i].y_pos + "px";
+      this.chemical[i].element.style.left = this.chemical[i].x_pos + "px";
+      this.chemical[i].element.style.backgroundColor = this.chemical[i].color;
+      this.chemical[i].element.style.height = this.chemical[i].radius * 2 + "px";
+      this.chemical[i].element.style.width = this.chemical[i].radius * 2 + "px";
+      if (this.chemical[i].distanced == false) {
+        this.chemical[i].x_pos = this.chemical[i].x_pos + this.chemical[i].x_velocity;
+        this.chemical[i].y_pos = this.chemical[i].y_pos + this.chemical[i].y_velocity;
+      }
+
+    }
+  }
 
 
 }
