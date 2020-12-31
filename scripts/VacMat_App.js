8a29a219f62edda1c4d2d94380225c2563fcc7d5
miguelangeltanem <mt73142@srcs.org>
2020-12-30 16:00:35 -0800

    generating

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -1,21 +1,29 @@
-document.addEventListener('DOMContentLoaded', () =>  {
-    const gridDisplay = document.querySelector('.grid')
-    const scoreDisplay = document.getElementById('score')
-    const resultDisplay = document.getElementById('result')
-    let squares = []
-    const width = 4
-    let score = 0
+document.addEventListener('DOMContentLoaded', () => {
+  const gridDisplay = document.querySelector('.grid')
+  const scoreDisplay = document.getElementById('score')
+  const resultDisplay = document.getElementById('result')
+  let squares = []
+  const width = 4
+  let score = 0
 
-    function createBoard() {
-        for (let i=0; i < width*width; i++) {
-          square = document.createElement('div')
-          square.innerHTML = 0
-          gridDisplay.appendChild(square)
-          squares.push(square)
-        }
-        generate()
-        generate()
-      }
-      createBoard()
-  
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      square = document.createElement('div')
+      square.innerHTML = PH
+      gridDisplay.appendChild(square)
+      squares.push(square)
+    }
+    generate()
+    generate()
+  }
+  createBoard()
+
+
+  function generate() {
+    randomNumber = Math.floor(Math.random() * squares.length)
+    if (squares[randomNumber].innerHTML == 0) {
+      squares[randomNumber].innerHTML = PH2
+      checkForGameOver()
+    } else generate()
+  }
 })
