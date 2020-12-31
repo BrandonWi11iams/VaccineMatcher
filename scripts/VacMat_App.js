48f13f50384ccd4f087d8482e4dc8d471e8541a7
miguelangeltanem <mt73142@srcs.org>
2020-12-30 16:07:58 -0800

    movement

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -26,4 +26,89 @@ document.addEventListener('DOMContentLoaded', () => {
       checkForGameOver()
     } else generate()
   }
+
+  function moveRight() {
+    for (let i=0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML
+        let totalTwo = squares[i+1].innerHTML
+        let totalThree = squares[i+2].innerHTML
+        let totalFour = squares[i+3].innerHTML
+        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+        let filteredRow = row.filter(num => num)
+        let missing = 4 - filteredRow.length
+        let zeros = Array(missing).fill(0)
+        let newRow = zeros.concat(filteredRow)
+
+        squares[i].innerHTML = newRow[0]
+        squares[i +1].innerHTML = newRow[1]
+        squares[i +2].innerHTML = newRow[2]
+        squares[i +3].innerHTML = newRow[3]
+      }
+    }
+  }
+
+  function moveLeft() {
+    for (let i=0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML
+        let totalTwo = squares[i+1].innerHTML
+        let totalThree = squares[i+2].innerHTML
+        let totalFour = squares[i+3].innerHTML
+        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+        let filteredRow = row.filter(num => num)
+        let missing = 4 - filteredRow.length
+        let zeros = Array(missing).fill(0)
+        let newRow = filteredRow.concat(zeros)
+
+        squares[i].innerHTML = newRow[0]
+        squares[i +1].innerHTML = newRow[1]
+        squares[i +2].innerHTML = newRow[2]
+        squares[i +3].innerHTML = newRow[3]
+      }
+    }
+  }
+
+
+  function moveUp() {
+    for (let i=0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML
+      let totalTwo = squares[i+width].innerHTML
+      let totalThree = squares[i+(width*2)].innerHTML
+      let totalFour = squares[i+(width*3)].innerHTML
+      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+      let filteredColumn = column.filter(num => num)
+      let missing = 4 - filteredColumn.length
+      let zeros = Array(missing).fill(0)
+      let newColumn = filteredColumn.concat(zeros)
+
+      squares[i].innerHTML = newColumn[0]
+      squares[i +width].innerHTML = newColumn[1]
+      squares[i+(width*2)].innerHTML = newColumn[2]
+      squares[i+(width*3)].innerHTML = newColumn[3]
+    }
+  }
+
+  function moveDown() {
+    for (let i=0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML
+      let totalTwo = squares[i+width].innerHTML
+      let totalThree = squares[i+(width*2)].innerHTML
+      let totalFour = squares[i+(width*3)].innerHTML
+      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+      let filteredColumn = column.filter(num => num)
+      let missing = 4 - filteredColumn.length
+      let zeros = Array(missing).fill(0)
+      let newColumn = zeros.concat(filteredColumn)
+
+      squares[i].innerHTML = newColumn[0]
+      squares[i +width].innerHTML = newColumn[1]
+      squares[i+(width*2)].innerHTML = newColumn[2]
+      squares[i+(width*3)].innerHTML = newColumn[3]
+    }
+  }
 })
