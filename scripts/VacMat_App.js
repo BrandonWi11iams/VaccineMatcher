18d256711d674891eff19a58657b46f890cbb218
Brandon Williams <bw73069@srcs.org>
2021-01-06 09:34:12 -0800

    redo

--- scripts/VacMat_App.js
+++ scripts/VacMat_App.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function createBoard() {
     for (let i = 0; i < width * width; i++) {
       square = document.createElement('div')
-      square.innerHTML = "*"
+      square.innerHTML = 0
       gridDisplay.appendChild(square)
       squares.push(square)
     }
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function generate() {
     randomNumber = Math.floor(Math.random() * squares.length)
-    if (squares[randomNumber].innerHTML == "*") {
+    if (squares[randomNumber].innerHTML == 0) {
       squares[randomNumber].innerHTML = 2
       checkForGameOver()
     } else generate()
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function moveRight() {
     for (let i = 0; i < 16; i++) {
-      if (i % 4 === "*") {
+      if (i % 4 === 0) {
         let totalOne = squares[i].innerHTML
         let totalTwo = squares[i + 1].innerHTML
         let totalThree = squares[i + 2].innerHTML
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function moveLeft() {
     for (let i = 0; i < 16; i++) {
-      if (i % 4 === "*") {
+      if (i % 4 === 0) {
         let totalOne = squares[i].innerHTML
         let totalTwo = squares[i + 1].innerHTML
         let totalThree = squares[i + 2].innerHTML
@@ -171,7 +171,7 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[i].innerHTML = combinedTotal
         squares[i + 1].innerHTML = 0
         score += combinedTotal
-        scoreDisplay.backgroundColor = score
+        scoreDisplay.innerHTML = score
       }
     }
     checkForWin()
@@ -184,14 +184,14 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[i].innerHTML = combinedTotal
         squares[i + width].innerHTML = 0
         score += combinedTotal
-        scoreDisplay.backgroundColor = score
+        scoreDisplay.innerHTML = score
       }
     }
     checkForWin()
   }
   function checkForWin() {
     for (let i = 0; i < squares.length; i++) {
-      if (squares[i].innerHTML == 512) {
+      if (squares[i].innerHTML == 2048) {
         resultDisplay.innerHTML = 'You WIN'
         document.removeEventListener('keyup', control)
         setTimeout(() => clear(), 3000)
